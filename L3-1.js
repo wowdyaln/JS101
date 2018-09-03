6b54773370c515a81d10e270860e4e8bd274a912
wowdyaln <rogersmk@gmail.com.tw>
2018-09-03 15:26:02 +0800

    L3-1 done.

---
+++ L3-1.js
@@ -0,0 +1,46 @@
+/*
+練習一：排序
+請寫一個 function sort，接收一個陣列並且回傳由小到大排序後的陣列。（禁止使用內建函式 sort）
+
+提示：如果你已經會找第 n 小的值了，試著把這個概念應用到這題上。
+
+sort([ 6, 8, 3, 2]) 預期回傳值：[2, 3, 6, 8]
+
+sort([ 1, 2, 7 ,5]) 預期回傳值：[1, 2, 5, 7]
+
+1。找到最小值，拿出來，並 push 到另一個 空array。
+2。重複 step1 ，直到拿完所有數值。
+
+*/
+function takeMin(array) {
+  let min = Math.min(...array)
+  let index = array.indexOf(min)
+  array.splice(index, 1)
+  return min
+}
+
+function sort(ary){
+  let ascendAry = []
+
+  function ascend(ay){
+    let minV = takeMin(ay)
+    ascendAry.push(minV)
+    if (ay.length !== 0){
+      return ascend(ay)
+    } else {
+      return ascendAry
+    }
+  }
+
+  return ascend(ary)
+}
+
+// TEST
+let list  = [6, 8, 3, 2,1,14,3,2,6,-1,-354,8,9,0,21,45,2,3,5,7,9]
+let list2 = [6, 8, 3, 2,1,14,3,2,6,-1,-354,8,9,0,21,45,2,3,5,7,9]
+
+let out = sort(list)
+let out2 = list2.sort( (a,b)=> a - b)
+
+console.log(out);
+console.log(out2);
---
+++ test.js
@@ -0,0 +1,12 @@
+
+let a = [1,2,3]
+
+// a.push(2,4,5,5,6,[2,1,[3,7,9],3])
+
+console.log(a);
+b= Array.isArray(a)
+console.log(b);
+c = [10,20]
+d = c.concat(a) 
+
+console.log(d);
