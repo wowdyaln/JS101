116565b187c259141380506a84e50d966582f75c
wowdyaln <rogersmk@gmail.com.tw>
2018-09-14 15:21:05 +0800

    babel-cli   babel-preset-env

--- temp.js
+++ temp.js
@@ -1,38 +1,4 @@
-function space(n) {
-  let space = ""
-  for (let i = 1; i < n; i++) {
-    space += "_"
-  }
-  // console.log(space);
-  return space
-}
+import kk ,{a, b, c} from './temp2'
 
-function leaf(n) {
-  let leaf = "*"
-  for (let i = 1; i < n; i++) {
-    leaf += "**"
-  }
-  // console.log(leaf);
-  return leaf
-}
-
-function printLowAll(n) {
-  let output = ""
-  for (let i = 1; i < n; i++) {
-    output += "_"
-  }
-  output += "*\n"
-
-  for (let k = 0; k < n; k++) {
-    console.log(output);
-  }
-}
-
-function tree(n) {
-  let upper = ""
-  for (let i = 0; i < n; i++) {
-    upper = space(n - i) + leaf(i + 1)
-    console.log(upper);
-  }
-  printLowAll(n)
-}
+console.log(kk(3,4) , a,b,c);
+// console.log(add.a, add.b, add.c, add(3,4));
---
+++ temp2.js
@@ -0,0 +1,13 @@
+
+ let a = 12
+ let b = 20
+let c = 30
+
+export default function add(a,b){
+  return a+b
+}
+
+export {
+  a,b,c
+}
+
